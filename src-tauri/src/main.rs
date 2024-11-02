83c0160a778ea3dee1c7009ee9b2458cf0399b9b
Michthebrand <michthebrand@gmail.com>
2024-11-02 09:15:37 +0100

    🛠: blurry text

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,14 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[cfg(windows)]
+extern crate winapi;
+
 fn main() {
+  #[cfg(target_os = "windows")]
+  unsafe {
+    winapi::um::shellscalingapi::SetProcessDpiAwareness(1);
+  }
   tauri::Builder::default()
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
