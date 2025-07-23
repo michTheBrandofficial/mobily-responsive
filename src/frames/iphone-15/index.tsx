cc5f4513a3ef07d8ff9d58ba36e74e4b466bc954
Mich_thedev <michthebrand@gmail.com>
2025-07-23 09:37:10 +0100

    [🚀 feat] added support for no bezels display for other iphone

--- src/frames/iphone-15/index.tsx
+++ src/frames/iphone-15/index.tsx
@@ -12,10 +12,9 @@ import { useScreenState } from "@/src/stores/screen-state";
 
 type Props = App.DeviceProps;
 
-// 352.467 and 717.433 are the dimensions which was tested for the iphone frame. It should be what we use to get our ratios for resizing of the iframe container;
 const dimensions = {
-  w: 393,
-  h: 852,
+	w: 393,
+	h: 852,
 };
 
 const deviceWidthRatio = 65 / dimensions.w;
@@ -31,63 +30,66 @@ const clothoidRadiusRatio = 49 / dimensions.w;
 const deviceBarRatios = [15 / dimensions.h, 6 / dimensions.h] as const;
 
 const Iphone15: Nixix.FC<Props> = ({ iframeSrc }): someView => {
-  const wrapperRef = ref<HTMLElement>();
-  const { iphoneConfig } = useIphoneConfig();
-  setupResizeEffect(wrapperRef, {
-    deviceBarRatios,
-    deviceHeightRatio,
-    deviceWidthRatio,
-    clothoidRadiusRatio,
-    virtualHomeButtonRatio,
-    safeAreaInsetRatio,
-  });
-  const { screenState } = useScreenState();
-  const backgroundMemo = memo(() => {
-    switch (screenState.value) {
-      case "after-app-launch":
-        return `tws-wallpaper-after-app-launch`;
-      case "before-close-app":
-        return `tws-wallpaper-iphone-15`;
-      default:
-        return `tws-wallpaper-iphone-15`;
-    }
-  }, [screenState]);
-  return (
-    <Wrapper bind:ref={wrapperRef}>
-      <DeviceFrame height={dimensions.h} />
-      <Container
-        className={concat`tws-h-auto tws-w-auto ${backgroundMemo}`}
-        style={{
-          ...pick(iphoneConfig, "width", "height"),
-          ...containerStyles,
-          clipPath: iphoneConfig.clothoidRadius,
-          backgroundSize: "cover",
-        }}
-      >
-        <Container
-          style={{
-            paddingTop: iphoneConfig.safeAreaInset,
-            width: percentage(100),
-            height: percentage(100),
-            clipPath: "inherit",
-            overflow: "hidden",
-            position: "relative",
-          }}
-        >
-          <StatusBar
-            style={{
-              width: iphoneConfig.width,
-              position: "absolute",
-              top: px(0),
-              zIndex: 900,
-            }}
-          />
-          <HomeScreen iframeSrc={iframeSrc} />
-          <AppScreen config="iphone" iframeSrc={iframeSrc} />
-        </Container>
-      </Container>
-    </Wrapper>
-  );
+	const wrapperRef = ref<HTMLElement>();
+	const { iphoneConfig } = useIphoneConfig();
+	setupResizeEffect(wrapperRef, {
+		deviceBarRatios,
+		deviceHeightRatio,
+		deviceWidthRatio,
+		clothoidRadiusRatio,
+		virtualHomeButtonRatio,
+		safeAreaInsetRatio,
+	});
+	const { screenState } = useScreenState();
+	const hasBezelsClassMemo = memo(() => {
+		return iphoneConfig.hasBezels!.value ? " " : " tws-invisible ";
+	}, [iphoneConfig.hasBezels!]);
+	const backgroundMemo = memo(() => {
+		switch (screenState.value) {
+			case "after-app-launch":
+				return `tws-wallpaper-after-app-launch`;
+			case "before-close-app":
+				return `tws-wallpaper-iphone-15`;
+			default:
+				return `tws-wallpaper-iphone-15`;
+		}
+	}, [screenState]);
+	return (
+		<Wrapper bind:ref={wrapperRef}>
+			<DeviceFrame height={dimensions.h} className={hasBezelsClassMemo} />
+			<Container
+				className={concat`tws-h-auto tws-w-auto ${backgroundMemo}`}
+				style={{
+					...pick(iphoneConfig, "width", "height"),
+					...containerStyles,
+					clipPath: iphoneConfig.clothoidRadius,
+					backgroundSize: "cover",
+				}}
+			>
+				<Container
+					style={{
+						paddingTop: iphoneConfig.safeAreaInset,
+						width: percentage(100),
+						height: percentage(100),
+						clipPath: "inherit",
+						overflow: "hidden",
+						position: "relative",
+					}}
+				>
+					<StatusBar
+						style={{
+							width: iphoneConfig.width,
+							position: "absolute",
+							top: px(0),
+							zIndex: 900,
+						}}
+					/>
+					<HomeScreen iframeSrc={iframeSrc} />
+					<AppScreen config="iphone" iframeSrc={iframeSrc} />
+				</Container>
+			</Container>
+		</Wrapper>
+	);
 };
 
 export default Iphone15;
