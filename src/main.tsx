bdbff82c01fe7efcb8b326037173bebc7f12e7b3
Mich_thedev <michthebrand@gmail.com>
2025-11-01 09:37:01 +0100

    [🚀 feat] optimized tsconfig.*

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import View from "./view";
 
+
 createRoot(document.querySelector<HTMLBodyElement>("body")!).render(
   <StrictMode>
     <View />
