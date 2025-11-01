df602dcd2b8c5b3377336f4974d079bf9c99b337
Mich_thedev <michthebrand@gmail.com>
2025-11-01 21:45:19 +0100

    [🚀 feat] fixed errors in homescreenicon

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import View from "./view";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 
 createRoot(document.querySelector<HTMLBodyElement>("body")!).render(
   <StrictMode>
-    <View />
+    <QueryClientProvider client={queryClient}>
+      <View />
+    </QueryClientProvider>
   </StrictMode>,
 );
