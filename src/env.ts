f5de6a6fcec0c21cc6a512ce61e23017f8ddcb41
Mich_thedev <michthebrand@gmail.com>
2025-12-01 00:31:39 +0100

    [🚀 feat] client env.ts file added

---
+++ src/env.ts
@@ -0,0 +1,4 @@
+export const EnvVariables = {
+  VITE_MOBILY_RESPONSIVE_VERSION: '0.7.0',
+  VITE_CHANGELOG_URL: 'https://github.com/mobily-responsive/mobily-responsive/releases'
+} satisfies Record<Capitalize<string>, string | number | boolean>;
