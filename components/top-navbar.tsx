2ba12a17d5f8c0651577a0f4452f655c89619759
Mich_thedev <michthebrand@gmail.com>
2025-12-01 20:36:22 +0100

    [🚀 feat] search button now has popover animation.

--- components/top-navbar.tsx
+++ components/top-navbar.tsx
@@ -1,14 +1,13 @@
 import { cn } from "@/lib/cn";
-import { useModalsBuilder } from "@/lib/modals-builder";
 import { uint8 } from "@/lib/number";
 import { pipe } from "@/lib/pipe";
 import {
-    inlineSwitch,
-    noop,
-    pick,
-    removeLeadingSlash,
-    separateProtocol,
-    sleep,
+  inlineSwitch,
+  noop,
+  pick,
+  removeLeadingSlash,
+  separateProtocol,
+  sleep,
 } from "@/lib/utils";
 import { useDeviceFrameHeight } from "@/src/constants";
 import { DEVICE_MAPPING } from "@/src/device-mapping";
@@ -21,7 +20,7 @@ import { appWindow as simulatorAppWindow } from "@tauri-apps/api/window";
 import { FormikProps, useFormik } from "formik";
 import { Check, Maximize2Icon, MinusIcon, XIcon } from "lucide-react";
 import { motion } from "motion/react";
-import React, { useEffect, useRef, useState } from "react";
+import React, { ReactNode, useEffect, useRef, useState } from "react";
 import { object, string } from "yup";
 import AppSettingsMenu from "./app-menu";
 import DeviceSelectMenu from "./device-select-menu";
@@ -33,7 +32,7 @@ import { Button } from "./ui/buttons";
 import { Input } from "./ui/inputs/input";
 import SearchableSelect from "./ui/inputs/searchable-select";
 import LiquidGlass from "./ui/liquid-glass";
-import Modal, { BaseModalProps } from "./ui/modal";
+import Popover from "./ui/popover";
 import { Typography } from "./ui/typography";
 
 const AnimatedCheckIcon = motion.create(Check);
@@ -52,7 +51,9 @@ const urlValidationSchema = object({
 
 const TopNavbar: React.FC = () => {
   const { setSrc: setIframeSrc, src: iframeSrc } = useIframeSrc();
-  const { storage: { lastUsedDevice: device } } = useLocalStorage()
+  const {
+    storage: { lastUsedDevice: device },
+  } = useLocalStorage();
   const deviceDisplayName = pick(
     DEVICE_MAPPING[device],
     "displayName",
@@ -66,16 +67,10 @@ const TopNavbar: React.FC = () => {
       },
     );
   });
-  const { modals, modalFunctions } = useModalsBuilder({
-    url: {
-      open: false,
-    },
-  });
-  const inputRef = useRef<HTMLTextAreaElement>(null);
   const { isFullscreen, setIsFullscreen } = useFullscreen();
   const { setScreenState } = useScreenState();
   const { setDeviceFrameHeightClass } = useDeviceFrameHeight();
-  const { setDeviceScreen, deviceScreen } = useDeviceScreen();
+  const { setDeviceScreen } = useDeviceScreen();
   const classMemo = isFullscreen ? "tws-hidden" : "tws-flex";
   const [mixingPercentage, setMixingPercentage] = useState(12);
   useEffect(() => {
@@ -171,36 +166,18 @@ const TopNavbar: React.FC = () => {
         >
           <Home className={"tws-size-5 tws-fill-white"} />
         </Button>
-        <Button
-          onTap={async () => {
-            modalFunctions.openModal("url", {});
-            await sleep(500);
-            inputRef.current?.focus();
-          }}
-          layout
-          initial={false}
-          animate={
-            deviceScreen === "home-screen"
-              ? {
-                  opacity: 0,
-                  width: 0,
-                  marginLeft: 0
-                }
-              : {
-                  opacity: 1,
-                  width: "auto",
-                  marginLeft: 16
-                }
+        <UrlInputPopover
+          trigger={
+            <SearchIcon className={"tws-size-5 tws-h-[18px] tws-fill-white"} />
           }
-          transition={{
-            delay: .9
+          formik={formik}
+          onSave={async () => {
+            setIframeSrc(
+              `${formik.values.protocol}${removeLeadingSlash(formik.values.url)}`,
+            );
           }}
-          className={cn("!tws-p-0 tws-bg-transparent ", {
-          })}
-        >
-          <SearchIcon className={"tws-size-5 tws-h-[18px] tws-fill-white"} />
-        </Button>
-        {/* device select popover here */}
+        />
+        {/* device select menu here */}
         <DeviceSelectMenu
           trigger={<DeviceFrameIcon className={"tws-size-5 tws-text-white "} />}
         />
@@ -208,137 +185,165 @@ const TopNavbar: React.FC = () => {
           trigger={<Settings className={"tws-size-5 tws-fill-white"} />}
         />
       </LiquidGlass.div>
-      <UrlModal
-        open={modals.url.open}
-        onClose={modalFunctions.returnClose("url")}
-        formik={formik}
-        inputRef={inputRef}
-        onSave={async () => {
-          setIframeSrc(
-            `${formik.values.protocol}${removeLeadingSlash(formik.values.url)}`,
-          );
-          await sleep(400);
-          modalFunctions.closeModal("url");
-        }}
-      />
     </section>
   );
 };
 
-interface UrlModalProps extends BaseModalProps {
+interface UrlInputPopoverProps {
+  trigger: ReactNode;
   formik: FormikProps<UrlFormikType>;
-  inputRef: React.RefObject<HTMLTextAreaElement | null>;
   onSave(): void;
 }
 
-const UrlModal: React.FC<UrlModalProps> = (props) => {
-  const { formik, open, onClose, onSave, inputRef } = props;
+const UrlInputPopover: React.FC<UrlInputPopoverProps> = (props) => {
+  const { formik, onSave } = props;
+  const { deviceScreen } = useDeviceScreen();
+  const inputRef = useRef<HTMLTextAreaElement>(null);
+
   return (
-    <Modal open={open} onClose={onClose}>
-      <Modal.Body className="tws-max-w-80 ">
-        <LiquidGlass.div
-          className="tws-p-5 tws-pt-6 tws-w-fit tws-rounded-[48px]  "
-          color={"#fff"}
-          mixingPercentage={80}
-        >
-          <div className="tws-flex tws-flex-col tws-gap-y-1.5 tws-px-1 ">
-            <Typography.h5 className="tws-text-lg tws-font-Switzer tws-text-zinc-900">
-              Add URL
-            </Typography.h5>
-            <Typography.p className="tws-text-base tws-leading-[26px] tws-w-full tws-font-Switzer tws-text-wrap tws-font-normal tws-text-zinc-900">
-              Enter your site's URL to preview it on this device.
-            </Typography.p>
-          </div>
-          <div className="tws-w-[280px] tws-h-fit tws-py-4 tws-px-6 tws-mt-6 tws-bg-zinc-300 tws-rounded-[32px] ">
-            <SearchableSelect
-              bottomBorder
-              required
-              className="tws-w-full "
-              placeholder="Protocol e.g HTTP"
-              options={[
-                { label: "HTTP", value: "http://" },
-                { label: "HTTPS", value: "https://" },
-              ]}
-              onChange={(value) =>
-                formik.setFieldValue("protocol", value?.value || "")
+    <Popover
+      transformOrigin="top"
+      onOpen={async () => {
+        await sleep(500);
+        inputRef.current?.focus();
+      }}
+      className="!tws-m-0"
+    >
+      <Popover.Trigger
+        className="tws-cursor-pointer"
+        layout
+        initial={false}
+        animate={
+          deviceScreen === "home-screen"
+            ? {
+                opacity: 0,
+                width: 0,
+                marginLeft: 0,
               }
-              value={formik.values.protocol}
+            : {
+                opacity: 1,
+                width: "auto",
+                marginLeft: 16,
+              }
+        }
+        transition={{
+          delay: 0.9,
+        }}
+      >
+        {props.trigger}
+      </Popover.Trigger>
+      <Popover.Content className="tws-max-w-80 tws-top-[200%] -tws-right-[300%] ">
+        {(closePopover) => {
+          return (
+            <LiquidGlass.div
+              className="tws-p-5 tws-pt-6 tws-w-fit tws-rounded-[48px]  "
+              color={"#fff"}
+              mixingPercentage={97}
             >
-              {(option, index) => (
-                <SearchableSelect.Option
-                  option={option}
-                  index={index}
-                  key={index}
-                  className="tws-pr-2"
+              <div className="tws-flex tws-flex-col tws-gap-y-1.5 tws-px-1 ">
+                <Typography.h5 className="tws-text-lg tws-font-Switzer tws-text-zinc-900">
+                  Add URL
+                </Typography.h5>
+                <Typography.p className="tws-text-base tws-leading-[26px] tws-w-full tws-font-Switzer tws-text-wrap tws-font-normal tws-text-zinc-900">
+                  Enter your site's URL to preview it on this device.
+                </Typography.p>
+              </div>
+              <div className="tws-w-[280px] tws-h-fit tws-py-4 tws-px-6 tws-mt-6 tws-bg-zinc-300 tws-rounded-[32px] ">
+                <SearchableSelect
+                  bottomBorder
+                  required
+                  className="tws-w-full "
+                  placeholder="Protocol e.g HTTP"
+                  options={[
+                    { label: "HTTP", value: "http://" },
+                    { label: "HTTPS", value: "https://" },
+                  ]}
+                  onChange={(value) =>
+                    formik.setFieldValue("protocol", value?.value || "")
+                  }
+                  value={formik.values.protocol}
                 >
-                  <div className="tws-flex tws-items-start tws-gap-x-2">
-                    <AnimatedCheckIcon
-                      size={16}
-                      variants={{
-                        hidden: { pathLength: 0, opacity: 0 },
-                        visible: {
-                          pathLength: 1,
-                          opacity: 1,
-                          transition: {
-                            pathLength: {
-                              delay: 0.2,
-                              type: "spring",
-                              duration: 1.5,
-                              bounce: 0,
+                  {(option, index) => (
+                    <SearchableSelect.Option
+                      option={option}
+                      index={index}
+                      key={index}
+                      className="tws-pr-2"
+                    >
+                      <div className="tws-flex tws-items-start tws-gap-x-2">
+                        <AnimatedCheckIcon
+                          size={16}
+                          variants={{
+                            hidden: { pathLength: 0, opacity: 0 },
+                            visible: {
+                              pathLength: 1,
+                              opacity: 1,
+                              transition: {
+                                pathLength: {
+                                  delay: 0.2,
+                                  type: "spring",
+                                  duration: 1.5,
+                                  bounce: 0,
+                                },
+                                opacity: { delay: 0.2, duration: 0.01 },
+                              },
                             },
-                            opacity: { delay: 0.2, duration: 0.01 },
-                          },
-                        },
-                      }}
-                      initial={"hidden"}
-                      animate={option.isSelected ? "visible" : "hidden"}
-                      className="tws-mt-1 "
-                    />
-                    <div className="tws-flex tws-flex-col t">
-                      <span className="tws-text-sm tws-font-medium">
-                        {option.label}
-                      </span>
-                      <span className="tws-text-xs tws-font-normal tws-text-zinc-600 "></span>
-                    </div>
-                    <span className="tws-text-xs tws-ml-auto tws-inline-block tws-px-2 tws-py-1 tws-rounded-full tws-bg-sky-50/50 tws-text-sky-500 tws-font-medium ">
-                      {option.value}
-                    </span>
-                  </div>
-                </SearchableSelect.Option>
-              )}
-            </SearchableSelect>
-            <Input.TextArea
-              inputRef={inputRef}
-              value={formik.values.url}
-              rows={2}
-              required
-              onChange={(e) => {
-                formik.setFieldValue("url", e.target.value);
-              }}
-              className="tws-caret-zinc-950 "
-              name="url"
-              placeholder="Url e.g acme.com"
-            />
-          </div>
-          <div className="tws-mt-6 tws-flex tws-items-center tws-gap-x-3 ">
-            <Button
-              onTap={onClose}
-              className="!tws-rounded-full !/tws-bg-[#bfb9c9] !tws-bg-zinc-300 tws-w-full tws-py-3.5"
-              variant="dormant"
-            >
-              Cancel
-            </Button>
-            <Button
-              onTap={onSave}
-              disabled={!formik.isValid}
-              className="!tws-rounded-full tws-w-full tws-py-3.5"
-            >
-              Ok
-            </Button>
-          </div>
-        </LiquidGlass.div>
-      </Modal.Body>
-    </Modal>
+                          }}
+                          initial={"hidden"}
+                          animate={option.isSelected ? "visible" : "hidden"}
+                          className="tws-mt-1 "
+                        />
+                        <div className="tws-flex tws-flex-col t">
+                          <span className="tws-text-sm tws-font-medium">
+                            {option.label}
+                          </span>
+                          <span className="tws-text-xs tws-font-normal tws-text-zinc-600 "></span>
+                        </div>
+                        <span className="tws-text-xs tws-ml-auto tws-inline-block tws-px-2 tws-py-1 tws-rounded-full tws-bg-sky-50/50 tws-text-sky-500 tws-font-medium ">
+                          {option.value}
+                        </span>
+                      </div>
+                    </SearchableSelect.Option>
+                  )}
+                </SearchableSelect>
+                <Input.TextArea
+                  inputRef={inputRef}
+                  value={formik.values.url}
+                  rows={2}
+                  required
+                  onChange={(e) => {
+                    formik.setFieldValue("url", e.target.value);
+                  }}
+                  className="tws-caret-zinc-950 "
+                  name="url"
+                  placeholder="Url e.g acme.com"
+                />
+              </div>
+              <div className="tws-mt-6 tws-flex tws-items-center tws-gap-x-3 ">
+                <Button
+                  onTap={closePopover}
+                  className="!tws-rounded-full !/tws-bg-[#bfb9c9] !tws-bg-zinc-300 tws-w-full tws-py-3.5"
+                  variant="dormant"
+                >
+                  Cancel
+                </Button>
+                <Button
+                  onTap={async () => {
+                    closePopover()
+                    await sleep(300)
+                    onSave()
+                  }}
+                  disabled={!formik.isValid}
+                  className="!tws-rounded-full tws-w-full tws-py-3.5"
+                >
+                  Ok
+                </Button>
+              </div>
+            </LiquidGlass.div>
+          );
+        }}
+      </Popover.Content>
+    </Popover>
   );
 };
 
--- components/ui/popover.tsx
+++ components/ui/popover.tsx
@@ -1,10 +1,6 @@
-import { pick, sleep } from "@/lib/utils";
+import { isFunction, pick, sleep } from "@/lib/utils";
 import { cn } from "@/lib/cn";
-import {
-  AnimatePresence,
-  HTMLMotionProps,
-  motion,
-} from "motion/react";
+import { AnimatePresence, HTMLMotionProps, motion } from "motion/react";
 import React, {
   createContext,
   Dispatch,
@@ -20,16 +16,16 @@ type PopoverContextType = {
   setOpen: Dispatch<SetStateAction<PopoverContextType["open"]>>;
   config: {
     transformOrigin:
-    | "center"
-    | "top"
-    | "bottom"
-    | "top-left"
-    | "top-right"
-    | "bottom-left"
-    | "bottom-right";
+      | "center"
+      | "top"
+      | "bottom"
+      | "top-left"
+      | "top-right"
+      | "bottom-left"
+      | "bottom-right";
   };
   onClose?: VoidFunction;
-  onOpen?: VoidFunction
+  onOpen?: VoidFunction;
 };
 
 const PopoverContext = createContext<PopoverContextType | null>(null);
@@ -42,14 +38,15 @@ const usePopover = () => {
   return popoverContent;
 };
 
-type PopoverProps = Pick<Props, "children"> & Pick<PopoverContextType, 'onClose' | 'onOpen'> & {
-  transformOrigin?:
-  PopoverContextType['config']['transformOrigin'];
-};
+type PopoverProps = Pick<Props, "children" | 'className'> &
+  Pick<PopoverContextType, "onClose" | "onOpen"> & {
+    transformOrigin?: PopoverContextType["config"]["transformOrigin"];
+  };
 
 const PopoverProvider = ({
   children,
   transformOrigin = "center",
+  className,
   ...props
 }: PopoverProps) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -61,10 +58,12 @@ const PopoverProvider = ({
         config: {
           transformOrigin,
         },
-        ...pick(props, 'onClose', 'onOpen')
+        ...pick(props, "onClose", "onOpen"),
       }}
     >
-      <section className={cn("w-fit h-fit relative")}>{children}</section>
+      <section className={cn(className, "tws-w-fit tws-h-fit tws-relative")}>
+        {children}
+      </section>
     </PopoverContext.Provider>
   );
 };
@@ -79,9 +78,9 @@ type Props = {
  */
 const PopoverTrigger: React.FC<
   Pick<Props, "children" | "className"> &
-  HTMLMotionProps<"div"> & {
-    triggerRef?: React.Ref<HTMLDivElement>;
-  }
+    HTMLMotionProps<"div"> & {
+      triggerRef?: React.Ref<HTMLDivElement>;
+    }
 > = ({ children, className, triggerRef, ...props }) => {
   const { setOpen, open, onOpen } = usePopover();
   const [shouldHaveHighZIndex, setShouldHaveHighZIndex] = useState(false);
@@ -90,17 +89,18 @@ const PopoverTrigger: React.FC<
   }, [open]);
   return (
     <motion.div
-      whileTap={{ scale: 0.95 }}
       {...props}
+      whileTap={{ scale: 0.9 }}
+      whileHover={{ scale: 0.9 }}
       ref={triggerRef}
-      className={cn("w-fit h-fit ", className, {
-        "relative z-[100000000]": shouldHaveHighZIndex,
+      className={cn("tws-w-fit tws-h-fit ", className, {
+        "tws-relative tws-z-[100000000]": shouldHaveHighZIndex,
       })}
       onTap={async () => {
         setShouldHaveHighZIndex(true);
-        await sleep(50)
+        await sleep(50);
         setOpen(true);
-        onOpen?.()
+        onOpen?.();
       }}
     >
       {children}
@@ -119,7 +119,7 @@ const PopoverClose: React.FC<
   const { setOpen } = usePopover();
   return (
     <div
-      className={cn("w-fit h-fit ", className)}
+      className={cn("tws-w-fit tws-h-fit ", className)}
       onClick={() => (onClose ? onClose(() => setOpen(false)) : setOpen(false))}
     >
       {children}
@@ -127,73 +127,85 @@ const PopoverClose: React.FC<
   );
 };
 
+type PopoverContentProps = Omit<Props, "children"> & {
+  children?: React.ReactNode | ((closePopover: VoidFunction) => React.ReactNode);
+};
+
 /**
  * @dev adding classnames right-0, left-0 to change the position of the popover
  */
-const PopoverContent: React.FC<Props> = ({ children, className }) => {
+const PopoverContent: React.FC<PopoverContentProps> = ({
+  children,
+  className,
+}) => {
   const { open, setOpen, config, onClose } = usePopover();
   const containerRef = useRef<HTMLElement>(null);
   useEffect(() => {
     if (open) containerRef.current?.focus();
   }, [open]);
   return (
-    <AnimatePresence mode="wait" >
+    <AnimatePresence mode="wait">
       {open ? (
         <>
           <motion.section
-            key={'popover-underlay'}
+            key={"popover-underlay"}
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{
               opacity: 0,
               transition: {
-                duration: 0.2
-              }
+                duration: 0.2,
+              },
             }}
             onClick={(e) => {
               if (e.target === e.currentTarget) (setOpen(false), onClose?.());
             }}
-            className="fixed h-screen w-screen bg-white/25 backdrop-blur-[2px] !mt-0 top-0 left-0 z-[10000000]"
+            className="tws-fixed tws-h-screen tws-w-screen tws-bg-transparent !tws-mt-0 tws-top-0 tws-left-0 tws-z-[10000000]"
           ></motion.section>
           <motion.section
             ref={containerRef}
-            key={'popover'}
+            key={"popover"}
             initial={{ scale: 0.8, opacity: 0 }}
             animate={{
               scale: 1,
               opacity: 1,
               transition: {
-                type: 'keyframes',
-                duration: 0.2
-              }
+                type: "keyframes",
+                duration: 0.2,
+              },
             }}
             exit={{
               scale: 0.8,
               opacity: 0,
               transition: {
                 duration: 0.25,
-                ease: "easeInOut"
-              }
+                ease: "easeInOut",
+              },
             }}
             tabIndex={0}
             className={cn(
-              `min-h-[400px] min-w-[300px] top-[120%] bg-white rounded-3xl absolute z-[100000000] border border-gray-100`,
               className,
-              { "origin-center": config.transformOrigin === "center" },
-              { "origin-top": config.transformOrigin === "top" },
-              { "origin-bottom": config.transformOrigin === "bottom" },
-              { "origin-top-right": config.transformOrigin === "top-right" },
-              { "origin-top-left": config.transformOrigin === "top-left" },
+              `tws-top-[120%] tws-absolute tws-z-[100000000] `,
+              { "tws-origin-center": config.transformOrigin === "center" },
+              { "tws-origin-top": config.transformOrigin === "top" },
+              { "tws-origin-bottom": config.transformOrigin === "bottom" },
               {
-                "origin-bottom-left": config.transformOrigin === "bottom-left",
+                "tws-origin-top-right": config.transformOrigin === "top-right",
+              },
+              { "tws-origin-top-left": config.transformOrigin === "top-left" },
+              {
+                "tws-origin-bottom-left":
+                  config.transformOrigin === "bottom-left",
               },
               {
-                "origin-bottom-right":
+                "tws-origin-bottom-right":
                   config.transformOrigin === "bottom-right",
-              }
+              },
             )}
           >
-            {children}
+            {isFunction(children)
+              ? children(() => (setOpen(false), onClose?.()))
+              : children}
           </motion.section>
         </>
       ) : (
