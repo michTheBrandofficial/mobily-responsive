5eabbb596ab0ee2a9403abe969c027184c252685
Mich_thedev <michthebrand@gmail.com>
2025-12-03 15:33:13 +0100

    [🚀 feat] version bump to 0.8.1

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,4 @@
 export const EnvVariables = {
-  VITE_MOBILY_RESPONSIVE_VERSION: '0.8.0',
+  VITE_MOBILY_RESPONSIVE_VERSION: '0.8.1',
   VITE_CHANGELOG_URL: 'https://github.com/mobily-responsive/mobily-responsive/releases'
 } satisfies Record<Capitalize<string>, string | number | boolean>;
