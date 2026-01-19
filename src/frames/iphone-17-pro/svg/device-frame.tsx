7b904a2166edba679ddb646fde8b7b51e420d417
themaker <michthebrand@gmail.com>
2026-01-19 23:56:50 +0100

    superellipse(1.85) used for frame corners

--- src/frames/iphone-17-pro/svg/device-frame.tsx
+++ src/frames/iphone-17-pro/svg/device-frame.tsx
@@ -5,22 +5,17 @@ import { FC } from "react";
 interface Props extends App.SVGProps {}
 
 const controlsShadows = {
-	// inner shadow with x -4, y 0, blur 5 spread -1 and color black opacity .95
-	// another inner shadow with x 0, y -4, blur 5 spread 0 and white opacity 22
-	// last shadow inner with x 0, y 4, blur 5, spread 0 and white opacity 22
-	controlSilence:
-		"tws-shadow-[inset_-4px_0px_5px_-1px_rgba(0,0,0,0.95),inset_0px_-4px_5px_0px_rgba(255,255,255,0.22),inset_0px_4px_5px_0px_rgba(255,255,255,0.22)]",
+	controlSilence: `tws-shadow-[-1px_0px_1px_0.3px_rgba(231,229,228,0.6),_-1px_0px_1px_1.5px_rgba(0,0,0,0.8)] `,
 	controlVolumeUp: function get() {
 		// uses the same shadow because on the same side
 		return this.controlSilence;
 	},
 	controlVolumeDown: function get() {
 		// uses the same shadow because on the same side
-		return this.controlVolumeUp;
+		return this.controlVolumeUp();
 	},
 	// reverse x position of first shadow in controlSilence
-	controlSideButton:
-		"tws-shadow-[inset_4px_0px_5px_-1px_rgba(0,0,0,0.95),inset_0px_-4px_5px_0px_rgba(255,255,255,0.22),inset_0px_4px_5px_0px_rgba(255,255,255,0.22)]",
+	controlSideButton: `tws-shadow-[1px_0px_1px_0.3px_rgba(231,229,228,0.6),_1px_0px_1px_1.5px_rgba(0,0,0,0.8)] `,
 };
 
 // scale of things from design is 3 so we divide by 3
@@ -28,38 +23,39 @@ const Controls: FC<Props> = () => {
 	return (
 		<>
 			{/* control silence */}
-			<devsize.div
+			<div
 				className={cn(
-					"tws-w-[calc(14px/3)] tws-h-[calc(103px/3)] tws-rounded-l-[calc(6px/3)] tws-bg-[#262427] ",
+					"tws-w-[1px] tws-h-[24px] tws-rounded-l-[1px] tws-bg-[#2C2B31] ",
 					// shadow for depth
 					controlsShadows.controlSilence,
-					"tws-hidden",
+					"tws-absolute -tws-left-[3px] tws-top-[112px] ",
 				)}
 			/>
 			{/* control volume up */}
-			<devsize.div
+			<div
 				className={cn(
-					"tws-w-[calc(14px/3)] tws-h-[calc(184px/3)] tws-rounded-l-[calc(6px/3)] tws-bg-[#262427] ",
+					"tws-w-[1px] tws-h-[45px] tws-rounded-l-[1px] tws-bg-[#2C2B31] ",
 					// shadow for depth
-					controlsShadows.controlVolumeUp,
-					"tws-hidden",
+					controlsShadows.controlVolumeUp(),
+					"tws-absolute -tws-left-[3px] tws-top-[155px] ",
 				)}
 			/>
-			{/* control volume up */}
-			<devsize.div
+			{/* control volume down */}
+			<div
 				className={cn(
-					"tws-w-[calc(14px/3)] tws-h-[calc(184px/3)] tws-rounded-l-[calc(6px/3)] tws-bg-[#262427] ",
+					"tws-w-[1px] tws-h-[45px] tws-rounded-l-[1px] tws-bg-[#2C2B31] ",
 					// shadow for depth
-					controlsShadows.controlVolumeDown,
-					"tws-hidden",
+					controlsShadows.controlVolumeDown(),
+					"tws-absolute -tws-left-[3px] tws-top-[212px] ",
 				)}
 			/>
 			{/* control side button */}
 			<devsize.div
 				className={cn(
-					"tws-w-[calc(14px/3)] tws-h-[calc(298px/3)] tws-rounded-r-[calc(6px/3)] tws-bg-[#262427] ",
+					"tws-w-[1px] tws-h-[72px] tws-rounded-r-[1px] tws-bg-[#2C2B31] ",
 					// shadow for depth
 					controlsShadows.controlSideButton,
+					"tws-absolute -tws-right-[3px] tws-top-[calc(185px)] ",
 				)}
 			/>
 		</>
@@ -68,13 +64,13 @@ const Controls: FC<Props> = () => {
 
 const DeviceFrame: FC<Props> = () => {
 	return (
-		<div
+		<devsize.div
 			id="main-container"
 			className="tws-w-fit tws-h-full tws-relative tws-aspect-[655/1363] tws-flex tws-items-center tws-justify-center [container-type:inline-size] "
 			style={{
 				// @ts-ignore
 				// Set based on parent container (which wraps touchable-screen)
-				"--base-radius": "calc(98 / 310 * 100cqw)",
+				"--base-radius": "calc(90 / 310 * 100cqw)",
 			}}
 		>
 			<div
@@ -82,6 +78,9 @@ const DeviceFrame: FC<Props> = () => {
 					"tws-h-full tws-w-fit tws-rounded-[calc(var(--base-radius)+35px/3)] ",
 					`tws-shadow-[0px_0px_1px_0.8px_rgba(231,229,228,0.6),_0px_0px_1px_2px_rgba(0,0,0,0.8)] `,
 				)}
+				style={{
+					cornerShape: "superellipse(1.85)",
+				}}
 			>
 				{/* Dark frame */}
 				<div
@@ -89,6 +88,9 @@ const DeviceFrame: FC<Props> = () => {
 						"tws-h-full tws-w-fit tws-p-[calc(13px/3)] tws-bg-[#2C2B31] ",
 						"tws-rounded-[calc(var(--base-radius)+33px/3)] ",
 					)}
+					style={{
+						cornerShape: "inherit",
+					}}
 				>
 					{/* Black Bezel */}
 					<div
@@ -96,6 +98,9 @@ const DeviceFrame: FC<Props> = () => {
 							"tws-h-full tws-w-fit tws-p-[calc(20px/3)] tws-bg-[#060100] ",
 							"tws-rounded-[calc(var(--base-radius)+20px/3)] ",
 						)}
+						style={{
+							cornerShape: "inherit",
+						}}
 					>
 						{/* device touchable screen */}
 						<div
@@ -104,12 +109,15 @@ const DeviceFrame: FC<Props> = () => {
 								"tws-h-full tws-w-fit tws-aspect-[201/437] tws-max-w-[402px] tws-max-h-[874px] tws-bg-white ",
 								"tws-rounded-[calc(var(--base-radius))] ",
 							)}
+							style={{
+								cornerShape: "inherit",
+							}}
 						></div>
 					</div>
 				</div>
 			</div>
 			<Controls />
-		</div>
+		</devsize.div>
 	);
 };
 
