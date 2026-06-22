b7a05911baaaacdd10694bf7348a3a52344cd0f5
themaker <michthebrand@gmail.com>
2026-06-22 12:01:20 +0100

    left ui done

--- src/left.view.tsx
+++ src/left.view.tsx
@@ -1,8 +1,12 @@
 import { Button } from "@/components/ui/buttons";
 import LiquidGlass from "@/components/ui/liquid-glass/liquid-glass";
 import { cn } from "@/lib/cn";
-// @ts-expect-error
-import { PauseFill, PlayFill, Multiply } from "framework7-icons/react";
+import {
+  PauseFill,
+  PlayFill,
+  Multiply,
+  // @ts-expect-error
+} from "framework7-icons/react";
 import { AnimatePresence, motion } from "motion/react";
 import { CSSProperties, useEffect, useMemo, useState } from "react";
 
@@ -25,6 +29,12 @@ function formatSecsAndMins(seconds: number) {
   return `${mins}:${secs}`;
 }
 
+/**
+ * @dev we don't allow past 1 hour;
+ */
+const MAX_SECONDS = 3599;
+const TIME_FOR_VISUAL_FEEDBACK = 3540;
+
 const View = () => {
   const [seconds, setSeconds] = useState(0);
   // default false, because we have to play it
@@ -35,7 +45,14 @@ const View = () => {
   useEffect(() => {
     if (!isRunning) return;
     const timer = setInterval(() => {
-      setSeconds((s) => s + 1);
+      setSeconds((s) => {
+        const newSeconds = s + 1;
+        if (newSeconds >= MAX_SECONDS) {
+          setIsRunning(false); // Stop the timer
+          return MAX_SECONDS; // Cap at 1 hour
+        }
+        return newSeconds;
+      });
     }, 1000); // 1 second
     return () => {
       clearInterval(timer);
@@ -43,13 +60,13 @@ const View = () => {
   }, [isRunning]);
 
   return (
-    <section className="tws-w-full tws-max-w-[320px] tws-p-2 ">
+    <section className="tws-w-full tws-select-none tws-max-w-[320px] tws-p-2 ">
       <LiquidGlass.div
-        frost={0.53}
+        frost={seconds < TIME_FOR_VISUAL_FEEDBACK ? 0.53 : 0.35}
         className={cn(
           "tws-h-fit tws-w-full tws-rounded-[120px] tws-overflow-hidden ",
           " tws-px-4 tws-py-3 tws-font-medium tws-flex tws-justify-between",
-          " tws-flex "
+          " tws-flex !tws-shadow-none "
         )}
         style={{
           // @ts-ignore
@@ -86,11 +103,19 @@ const View = () => {
         <div
           data-tauri-drag-region
           style={{ "app-region": "drag" } as CSSProperties}
-          className="tws-flex-grow tws-bg-transparent tws-h-11 "
+          className="tws-flex-grow tws-bg-transparent tws-h-11 tws-cursor-grab "
         ></div>
-        <div className="tws-flex tws-font-SF_Pro_Display tws-items-end tws-gap-1 tws-ml-auto">
-          <p className="tws-font-medium tws-text-amber-500">Timer</p>
-          <p className="tws-font-normal tws-text-3xl tws-text-amber-500 tws-tabular-nums">
+        <div
+          className={cn(
+            "tws-flex tws-font-SF_Pro_Display tws-items-end tws-gap-1 tws-ml-auto",
+            {
+              "tws-text-amber-500": seconds < TIME_FOR_VISUAL_FEEDBACK,
+              "tws-text-red-700": seconds >= TIME_FOR_VISUAL_FEEDBACK,
+            }
+          )}
+        >
+          <p className="tws-font-medium tws-transition-colors ">Timer</p>
+          <p className="tws-font-normal tws-text-3xl tws-tabular-nums tws-transition-colors">
             {formattedTime}
           </p>
         </div>
